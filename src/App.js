5d59984ac61f169390de0813ce7abe251a6be60b
BinduBhargaviVadla-WAL <bindu.v@westagilelabs.com>
2022-02-28 19:05:31 +0530

    Feb-28 assignment added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Hobbies from './Hobbies';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Hobbies/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/HobbiesReducer.js
@@ -0,0 +1,14 @@
+const HobbiesReducer = (state,action) => {
+    console.log(state);
+    console.log(action);
+    if(action.type == "add"){
+        return [...state,action.name]
+    }
+    if(action.type == "clearAll"){
+        return []
+    }
+    if(action.type == "deleteIndex"){
+        return state.filter((val,index) => index != action.index)
+    }
+}
+export default HobbiesReducer;
